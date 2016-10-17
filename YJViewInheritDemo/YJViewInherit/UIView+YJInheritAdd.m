1861f60341b671543e4246351b9d086a6e27e47d
Alex <wang300687@sina.com>
2016-10-17 17:09:15 +0800

    新增子view存在xib的情况

--- YJViewInheritDemo/YJViewInherit/UIView+YJInheritAdd.m
+++ YJViewInheritDemo/YJViewInherit/UIView+YJInheritAdd.m
@@ -19,29 +19,37 @@
     //runtime获取父类
     Class superClass = class_getSuperclass([self class]);
     NSString *sClassName = NSStringFromClass(superClass);
-    UIView *superView = (UIView*)[[[NSBundle mainBundle] loadNibNamed:sClassName owner:self options:nil] lastObject];
     
-    //初始化view属性
-    [self setFrame:superView.frame];
+    //获取子类，验证子类是否存在xib
+    NSString *sSelfClassName = NSStringFromClass([self class]);
+    NSArray *selfXib = [[NSBundle mainBundle] loadNibNamed:sSelfClassName owner:self options:nil];
     
-    self.backgroundColor = [superView backgroundColor];
-    
-    //取出父类成员，并加进子类中
-    unsigned propertyCount = 0;
-    objc_property_t *properties = class_copyPropertyList([superView class], &propertyCount);
-    for ( int i = 0 ; i < propertyCount ; i++ ) {
-        objc_property_t property = properties[i];
-        NSString *propertyName = [[NSString alloc] initWithCString:property_getName(property) encoding:NSUTF8StringEncoding];
-        if (propertyName)
-        {
-            //父类控件成员
-            UIView *subView = [superView valueForKey:propertyName];
-            [self setValue:subView forKey:propertyName];
+    //存在则利用同名xib进行实例
+    if (selfXib.count > 0) {
+        self = [selfXib lastObject];
+    } else {
+        UIView *superView = (UIView*)[[[NSBundle mainBundle] loadNibNamed:sClassName owner:self options:nil] lastObject];
+        //初始化view属性
+        [self setFrame:superView.frame];
+        
+        self.backgroundColor = [superView backgroundColor];
+        
+        //取出父类成员，并加进子类中
+        unsigned propertyCount = 0;
+        objc_property_t *properties = class_copyPropertyList([superView class], &propertyCount);
+        for ( int i = 0 ; i < propertyCount ; i++ ) {
+            objc_property_t property = properties[i];
+            NSString *propertyName = [[NSString alloc] initWithCString:property_getName(property) encoding:NSUTF8StringEncoding];
+            if (propertyName)
+            {
+                //父类控件成员
+                UIView *subView = [superView valueForKey:propertyName];
+                [self setValue:subView forKey:propertyName];
+            }
         }
+        
+        [self addSubview:superView];
     }
-    
-    [self addSubview:superView];
-    
     return self;
 }
 
